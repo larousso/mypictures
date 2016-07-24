59a1b786749ebb425ce10e7a342d4ce1de49bfa2
adelegue <adelegue@hotmail.com>
2016-07-24 17:42:15 +0200

    bugs

--- src/component/theme.js
+++ src/component/theme.js
@@ -1,5 +1,5 @@
 import {cyan500, cyan700, lightBlack, red700, grey500, darkBlack, white, grey300} from 'material-ui/styles/colors';
-import ColorManipulator from 'material-ui/lib/utils/color-manipulator';
+import ColorManipulator from 'material-ui/utils/color-manipulator';
 import Spacing from 'material-ui/styles/spacing';
 import zIndex from 'material-ui/styles/zIndex';
 
