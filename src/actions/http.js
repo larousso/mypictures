933f59c7b0b926d18cdfdd33a001719afa5fc2ca
adelegue <adelegue@hotmail.com>
2016-11-11 10:49:31 +0100

    Bugfix

--- src/actions/http.js
+++ src/actions/http.js
@@ -9,20 +9,31 @@ const options = {
     }
 };
 
-const getOptions = sessionData => {
-    if(sessionData) {
-        const regExp = /^(.*)-user=(.*)/gm;
-        const parsed = regExp.exec(sessionData);
-        if(parsed.length > 2) {
-            const hash = parsed[1];
-            const session = parsed[2];
-            const cookie = `_sessiondata=${hash}-user=${encodeURIComponent(session)};`;
-            const headers = {...options.headers, 'Cookie': cookie};
-            return {headers, credentials: 'include', mode: 'cors'};
-        }
+const getOptions = (sessionData, encoded = false) => {
+    const cookie = buildSession(sessionData, encoded);
+    if(cookie) {
+        console.log('Cookie', cookie);
+        const headers = {...options.headers, 'Cookie': cookie};
+        return {headers, credentials: 'include', mode: 'cors'};
     }
     return options;
 };
+
+function buildSession(sessionData, encoded) {
+    if(sessionData) {
+        const regExp = /^(.*)-user=(.*)/gm;
+        const parsed = regExp.exec(sessionData);
+        if (parsed.length > 2) {
+            const hash = parsed[1];
+            const session = parsed[2];
+            if (encoded) {
+                return `_sessiondata=${hash}-user=${session};`;
+            } else {
+                return `_sessiondata=${hash}-user=${encodeURIComponent(session)};`;
+            }
+        }
+    }
+}
 
 const buildUrl = (path) => {
     return clientConfig.api.baseUrl + path;
@@ -36,10 +47,10 @@ function handleErrors(response) {
 }
 
 export default {
-    get(url, sessionData) {
+    get(url, sessionData, encoded) {
         return fetch(buildUrl(url), {
             method: 'get',
-            ...getOptions(sessionData)
+            ...getOptions(sessionData, encoded)
         })
         .then(handleErrors)
         .then(rep => rep.json());
@@ -47,8 +58,8 @@ export default {
     post(url, body, sessionData) {
         return this.rawPost(url, body, sessionData).then(rep => rep.json());
     },
-    rawPost(url, body, sessionData) {
-        const theOptions = getOptions(sessionData);
+    rawPost(url, body, sessionData, encoded) {
+        const theOptions = getOptions(sessionData, encoded);
         console.log('posting {} to {} with {}', body, url, theOptions);
         return fetch(buildUrl(url), {
             method: 'post',
@@ -57,10 +68,10 @@ export default {
         })
         .then(handleErrors);
     },
-    postData(url, data, sessionData) {
+    postData(url, data, sessionData, encoded) {
         let headers = {};
         if(sessionData) {
-            headers.cookie = `_sessiondata=${sessionData};`;
+            headers.cookie = buildSession(sessionData, encoded);
         }
         return fetch(buildUrl(url), {
             method: 'post',
